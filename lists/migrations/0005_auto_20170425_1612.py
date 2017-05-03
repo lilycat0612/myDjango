5daab5271685f03dd0f18c050017d3662ad63331
lily <lily>
2017-05-03 12:19:42 +0800

    section 7.7 finished

---
+++ lists/migrations/0005_auto_20170425_1612.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('lists', '0004_auto_20170425_1609'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='item',
+            name='list',
+            field=models.ForeignKey(default='', to='lists.List'),
+        ),
+    ]
