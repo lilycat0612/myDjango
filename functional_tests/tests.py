6a784885d920276a15142874074bd8072aa88594
lilycat0612 <27803532@qq.com>
2017-08-30 14:34:38 +0800

    First

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -5,10 +5,28 @@ from selenium.webdriver.common.keys import Keys
 import time
 #from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+import sys
 
 
 #class NewVisitorTest(LiveServerTestCase):
 class NewVisitorTest(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://'+arg.split('=')[1]
+                return
+            
+        super().setUpClass()
+        cls.server_url=cls.live_server_url 
+     
+     
+    @classmethod
+    def tearDownClass(cls):
+        if   cls.server_url==cls.live_server_url: 
+            super.tearDownClass()  
+        
+               
     def setUp(self):
         self.browser = webdriver.Firefox()
         #self.browser.implicitly_wait(3)
@@ -25,7 +43,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     
     def test_can_start_a_list_and_retrieve_it_later(self):
     
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         time.sleep(10)
         
         self.assertIn('To-Do',self.browser.title)
@@ -55,7 +73,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser=webdriver.Firefox()
         time.sleep(10)
         
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         time.sleep(10)
         page_text=self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers',page_text)
@@ -78,7 +96,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_layout_and_styling(self):
     #error msg needed to fix:new_loc = {"x": round(old_loc['x']),TypeError: 'NoneType' object is not subscriptable
 
-#         self.browser.get(self.live_server_url)
+#         self.browser.get(self.server_url)
 #         self.browser.set_window_size(1024,768)
 #         time.sleep(10)
 #                 
